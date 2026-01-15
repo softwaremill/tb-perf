9a7f5727972a6b8d53efa7793cd45748e4014951
adamw <adam@warski.org>
2026-01-15 15:02:04 +0100

    Update scripts

--- scripts/run-postgres-sanity.sh
+++ scripts/run-postgres-sanity.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-./scripts/stop-docker.sh all
 cargo build --release
 cargo run --release --bin coordinator -- -c config.sanity-postgresql.toml
--- scripts/run-tigerbeetle-sanity.sh
+++ scripts/run-tigerbeetle-sanity.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-./scripts/stop-docker.sh all
 cargo build --release
 
-./scripts/tigerbeetle-local.sh wipe
 ./scripts/tigerbeetle-local.sh start
 docker compose -f docker/docker-compose.tigerbeetle.yml -p tbperf up -d otel-collector prometheus grafana
 
 ./target/release/coordinator -c config.sanity-tigerbeetle.toml --no-docker
+
+./scripts/tigerbeetle-local.sh wipe
+./scripts/stop-docker.sh tigerbeetle
